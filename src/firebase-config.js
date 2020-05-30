fb934e26c12f1206b7d73c72db469af28bf5bb25
Pyae Sone <mgmgpyaesonewin@gmail.com>
2020-05-31 03:39:35 +0630

    react fire resolved

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,13 @@
 */
 const firebaseConfig = {
   apiKey: "AIzaSyAyZOLu_Z3iQtFnMLgDaazDa5D44zrmkxo",
-    authDomain: "friendlymarket-e8e51.firebaseapp.com",
-    databaseURL: "https://friendlymarket-e8e51.firebaseio.com",
-    projectId: "friendlymarket-e8e51",
-    storageBucket: "friendlymarket-e8e51.appspot.com",
-    messagingSenderId: "516372538012",
-    appId: "1:516372538012:web:0af0fa3570c1f244394359",
-    measurementId: "G-MDEH4FG38T"
+  authDomain: "friendlymarket-e8e51.firebaseapp.com",
+  databaseURL: "https://friendlymarket-e8e51.firebaseio.com",
+  projectId: "friendlymarket-e8e51",
+  storageBucket: "friendlymarket-e8e51.appspot.com",
+  messagingSenderId: "516372538012",
+  appId: "1:516372538012:web:0af0fa3570c1f244394359",
+  measurementId: "G-MDEH4FG38T",
 };
 
 export default firebaseConfig;
